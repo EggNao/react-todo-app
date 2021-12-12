41112db0cc6866377523f694bb503194f27fc8cc
江口直輝 <eguchi.naoki900@mail.kyutech.jp>
2021-12-12 15:17:29 +0900

    add: Filter componentsの追加

---
+++ src/components/Filter.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import Box from '@mui/material/Box'
+import Tab from '@mui/material/Tab'
+import TabContext from '@mui/lab/TabContext'
+import TabList from '@mui/lab/TabList'
+
+const Filter = () => {
+  const [value, setValue] = React.useState('1')
+
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue)
+  }
+
+  return (
+    <Box sx={{ width: '100%', typography: 'body1' }}>
+      <TabContext value={value}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <TabList onChange={handleChange} aria-label="lab API tabs example">
+            <div style={{ margin: 'auto' }}>
+              <Tab label="all" value="1" />
+              <Tab label="todo" value="2" />
+              <Tab label="done" value="3" />
+            </div>
+          </TabList>
+        </Box>
+      </TabContext>
+    </Box>
+  )
+}
+export default Filter

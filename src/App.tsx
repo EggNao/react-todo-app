b0d64c3d8b16dfbaffc06c1c85bb88af5662f064
江口直輝 <eguchi.naoki900@mail.kyutech.jp>
2021-12-12 23:18:01 +0900

    add: Taskを配列に格納する機能を追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,74 @@
-import React from 'react'
-import './App.css'
+import React, { useState } from 'react'
 import TodoList from './components/TodoList'
 import Header from './components/Header'
 import Filter from './components/Filter'
-import InputTask from './components/InputTask'
+import Loading from './components/Loading'
+import Box from '@mui/material/Box'
+import TextField from '@mui/material/TextField'
+import Stack from '@mui/material/Stack'
+import Button from '@mui/material/Button'
+import { todo } from './types/type'
 
 function App() {
+  const [Items, setItems] = useState<todo[]>([])
+  const [TaskTexts, setTaskTexts] = useState<string>('')
+  const [count, setCount] = useState<number>(0)
+
+  const onClick = () => {
+    setCount(count + 1)
+    console.log(count)
+    console.log(TaskTexts)
+    const Items_cp = Items
+    Items_cp.push({ count: count, text: TaskTexts, done: false })
+    setItems(Items_cp)
+    console.log(Items)
+
+    setTaskTexts('')
+  }
+  const handleChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setTaskTexts(event.target.value)
+  }
+
   return (
     <div className="App">
       <Header />
-      <InputTask />
+      <Box
+        component="form"
+        sx={{
+          '& .MuiTextField-root': { m: 1, width: '25ch' },
+        }}
+        noValidate
+        autoComplete="off"
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <TextField
+          required
+          id="outlined-required"
+          label="Task"
+          defaultValue=""
+          onChange={handleChange}
+          value={TaskTexts}
+        />
+
+        <Stack spacing={2} direction="row">
+          <Button
+            variant="contained"
+            style={{ textTransform: 'none', height: '30px' }}
+            onClick={onClick}
+          >
+            Save
+          </Button>
+        </Stack>
+      </Box>
       <Filter />
       <TodoList />
+      <Loading />
     </div>
   )
 }

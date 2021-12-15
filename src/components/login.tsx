171aeda57c07e7070be505857f353332fd7971ed
江口直輝 <eguchi.naoki900@mail.kyutech.jp>
2021-12-15 21:33:38 +0900

    :lipstick: Login Component追加
    UIのみ作成した

---
+++ src/components/login.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Stack, Button } from '@mui/material'
+
+const Login = () => {
+  return (
+    <Stack spacing={2} direction="row">
+      <Button variant="text">Login</Button>
+    </Stack>
+  )
+}
+
+export default Login

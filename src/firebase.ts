ecb2d98d4e86517180472d669a854392f90e48b5
江口直輝 <eguchi.naoki900@mail.kyutech.jp>
2021-12-16 00:29:57 +0900

    :lipstick: Firebase Authを追加
    Firebase Authを使えるようにした

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,16 +1,22 @@
 import { initializeApp } from 'firebase/app'
+import {
+  getAuth,
+  GoogleAuthProvider
+} from 'firebase/auth'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  apiKey: 'AIzaSyAZf7p5te4vm0pAIEFst-rFgFtbYawwnf0',
+  authDomain: 'react-todo-app-5d426.firebaseapp.com',
+  projectId: 'react-todo-app-5d426',
+  storageBucket: 'react-todo-app-5d426.appspot.com',
+  messagingSenderId: '878067763188',
+  appId: '1:878067763188:web:94b66a04693d272d069ba1',
 }
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig)
+export const app = initializeApp(firebaseConfig)
+export const provider = new GoogleAuthProvider()
+export const auth = getAuth()

2b9e20e0714c2f6c383bd2f0e1ac8e4d4839d7e2
江口直輝 <eguchi.naoki900@mail.kyutech.jp>
2021-12-19 17:04:27 +0900

    :sparkles: DOMが破棄された時にsign out
    DOMが破棄された時にsign outするようにした

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,30 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import TodoList from './components/TodoList'
 import Header from './components/Header'
 import Filter from './components/Filter'
 import InputTask from './components/InputTask'
-import Loading from './components/Loading'
 import Login from './components/Login'
 import { todo } from './types/type'
+import { getAuth, signOut } from 'firebase/auth'
 
 function App() {
   const [Items, setItems] = useState<todo[]>([])
   const [id, setId] = useState<string>('')
   const [count, setCount] = useState<number>(0)
   const [isLogin, setIsLogin] = useState<boolean>(false)
+  useEffect(() => {
+    return () => {
+      const auth = getAuth()
+      signOut(auth)
+        .then(() => {
+          // Sign-out successful.
+          setIsLogin(false)
+        })
+        .catch((error) => {
+          // An error happened.
+        })
+    }
+  }, [])
 
   const page = () => {
     if (isLogin) {
